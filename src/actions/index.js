d12529e3afd2f4c3905e38e66af6e03c83355778
Glitch (redux-set-language-starting-point) <none>
2018-12-14 11:45:07 -0700

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
 // Add your code for SET_LANGUAGE and setLanguage
-export const SET_LANGUAGE = null;
-export const setLanguage = null;
+export const SET_LANGUAGE = 'SET_LANGUAGE';
+export const setLanguage = (language, listIndex) =>({
+type: SET_LANGUAGE,
+language,
+listIndex
+});
+
+// export const ADD_CARD = 'ADD_CARD';
+// export const addCard = (text, listIndex) => ({
+//     type: ADD_CARD,
+//     text,
+//     listIndex
+// });
--- src/store.js
+++ src/store.js
@@ -2,4 +2,6 @@ import {createStore} from 'redux';
 
 import {languageReducer} from './reducers';
 
+const store =createStore
+
 export default createStore(languageReducer);
